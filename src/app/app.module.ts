d09e3e9c8d56812ec7f7fb268270d8489826ad20
Pranav Jha <pranjha@deloitte.com>
2019-12-01 03:03:02 +0530

    added new component for rendering a cell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CellCustomComponent } from './cell-custom/cell-custom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellCustomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cell-custom/cell-custom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cell-custom',
+  templateUrl: './cell-custom.component.html',
+  styleUrls: ['./cell-custom.component.css']
+})
+export class CellCustomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
